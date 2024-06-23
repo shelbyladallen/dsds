c20011cb15b90026cc7a752cff978c208c69c4b0
Shelby Allen <shelby.lad.allen@gmail.com>
2024-06-23 14:00:30 -0400

    init commit with dialogic plugin (enable in the project settings!)

---
+++ main.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _input(event: InputEvent):
+	# check if a dialog is already running
+	if Dialogic.current_timeline != null:
+		return
+
+	if event is InputEventKey and event.keycode == KEY_ENTER and event.pressed:
+		Dialogic.start('main_story_timeline')
+		get_viewport().set_input_as_handled()
